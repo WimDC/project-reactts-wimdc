0f4acc449f8de98292f865c72621163abde11796
Tijl Ivens <tijl.ivens@gmail.com>
2023-06-01 20:13:31 +0200

    use react hook

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,18 +1,21 @@
 import { Box, Typography, Collapse, IconButton } from "@mui/material";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { useState, useEffect } from "react";
 import { WeatherBox, WeatherData } from "./Partial/WeatherBox";
 import { Dispatch, SetStateAction } from "react";
+import { useCycleColor } from "./Partial/useCycleColor";
 
-const getData = async (setData: Dispatch<SetStateAction<WeatherData | null>>) => {
+const getData = async (
+  setData: Dispatch<SetStateAction<WeatherData | null>>
+) => {
   const result = await fetch(
-    'https://weatherapi-com.p.rapidapi.com/current.json?q=Mechelen',
+    "https://weatherapi-com.p.rapidapi.com/current.json?q=Mechelen",
     {
-      method: 'GET',
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': 'b40d99d42dmsha3832b1327f844dp1f3a9bjsn9e8a7b3c1e0a',
-        'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-      }
+        "X-RapidAPI-Key": "b40d99d42dmsha3832b1327f844dp1f3a9bjsn9e8a7b3c1e0a",
+        "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+      },
     }
   );
   const resultJson = await result.json();
@@ -24,28 +27,8 @@ const getData = async (setData: Dispatch<SetStateAction<WeatherData | null>>) =>
 export const Home = () => {
   const [data, setData] = useState<WeatherData | null>(null);
   const [isExpanded, setExpanded] = useState(true);
-  
-  const magenta = '#FF00FF';
-  const yellow = '#FFFF00';
-  const cyan = '#00FFFF';
 
-  const [buttonColor, setButtonColor] = useState(yellow);
-
-  useEffect(() => {
-    const colorInterval = setInterval(() => {
-      setButtonColor((prevColor) => {
-        if (prevColor === yellow) {
-          return cyan;
-        } else if (prevColor === cyan) {
-          return magenta;
-        } else {
-          return yellow;
-        }
-      });
-    }, 1000);
-
-    return () => clearInterval(colorInterval);
-  }, []);
+  const buttonColor = useCycleColor();
 
   const toggleAccordion = () => {
     setExpanded(!isExpanded);
@@ -66,48 +49,52 @@ export const Home = () => {
         justifyContent: "center",
         height: "100vh",
         textAlign: "center",
-        backgroundImage: "url('https://myguitarlessons.co.uk/wp-content/uploads/2014/10/Kleinteilelag-Thomann-factory-620x360.jpg')",
+        backgroundImage:
+          "url('https://myguitarlessons.co.uk/wp-content/uploads/2014/10/Kleinteilelag-Thomann-factory-620x360.jpg')",
         backgroundSize: "cover",
-        backgroundPosition: "center"
+        backgroundPosition: "center",
       }}
+    >
+      <Box
+        sx={{
+          backgroundColor: "rgba(211, 211, 211, 0.5)",
+          backdropFilter: "blur(10px)",
+          borderRadius: "8px",
+        }}
       >
-          <Box sx={{backgroundColor: "rgba(211, 211, 211, 0.5)",
-        backdropFilter: "blur(10px)",
-        borderRadius: "8px",}}>
-      <Typography sx={{ fontSize: "100px", fontWeight: "bold", color: "white" }} variant="h2">
-        HOME
-      </Typography>
-      <Typography sx={{ fontSize: 40, marginBottom: 5, color: "white" }} variant="h3">
-        Stock-o-matic
-      </Typography>
-      <Typography sx={{ fontSize: 25, color: "white",  marginBottom: 0}}>
-                  Welcome to the stock management tool you need!
+        <Typography
+          sx={{ fontSize: "100px", fontWeight: "bold", color: "white" }}
+          variant="h2"
+        >
+          HOME
+        </Typography>
+        <Typography
+          sx={{ fontSize: 40, marginBottom: 5, color: "white" }}
+          variant="h3"
+        >
+          Stock-o-matic
+        </Typography>
+        <Typography sx={{ fontSize: 25, color: "white", marginBottom: 0 }}>
+          Welcome to the stock management tool you need!
         </Typography>
 
         <Box display="flex" alignItems="center" justifyContent="space-between">
-        <Typography variant="h6"></Typography>
-        <IconButton onClick={toggleAccordion} sx={{ color: buttonColor }}>
-          <ExpandMoreIcon />
-        </IconButton>
-      </Box>
-      <Collapse in={isExpanded}>
-        <Typography>
-            This app allows you to:
-          </Typography>
-          <Typography>
-            - view stocks and carstocks
-          </Typography>
+          <Typography variant="h6"></Typography>
+          <IconButton onClick={toggleAccordion} sx={{ color: buttonColor }}>
+            <ExpandMoreIcon />
+          </IconButton>
+        </Box>
+        <Collapse in={isExpanded}>
+          <Typography>This app allows you to:</Typography>
+          <Typography>- view stocks and carstocks</Typography>
           <Typography>
             - add items to or take items from a particular stock
           </Typography>
-          <Typography>
-            - view the log of all made transactions
-        </Typography>
+          <Typography>- view the log of all made transactions</Typography>
         </Collapse>
-        
-              {data?.location && <WeatherBox data={data} />}
-          </Box>
-          
+
+        {data?.location && <WeatherBox data={data} />}
+      </Box>
     </Box>
   );
 };
--- src/Pages/StockDetail/StockDetail.tsx
+++ src/Pages/StockDetail/StockDetail.tsx
@@ -23,8 +23,12 @@ export const StockDetail: FC = () => {
   const getStock = getStockContent();
   const [stockData, setStockData] = useState<StockProps[]>(getStock);
   const [stockLog, setStockLog] = useState<StockLogEntry[]>([]);
-  const stockById = stockData.filter((stockItem) => stockItem.stockId === stockId);
-  const stock = stockData.find((specificStock) => specificStock.stockId === stockId);
+  const stockById = stockData.filter(
+    (stockItem) => stockItem.stockId === stockId
+  );
+  const stock = stockData.find(
+    (specificStock) => specificStock.stockId === stockId
+  );
 
   useEffect(() => {
     const storedStockLog = localStorage.getItem("stockLog");
@@ -37,7 +41,11 @@ export const StockDetail: FC = () => {
     navigate("/request/" + stockId);
   };
 
-  const addStockLogEntry = (productName: string, productId: string | number, amount: number) => {
+  const addStockLogEntry = (
+    productName: string,
+    productId: string | number,
+    amount: number
+  ) => {
     const currentDate = new Date().toLocaleString();
     const newStockLogEntry: StockLogEntry = {
       productName,
@@ -52,16 +60,22 @@ export const StockDetail: FC = () => {
     localStorage.setItem("stockLog", JSON.stringify(updatedStockLog));
   };
 
-  const withdrawOne = (stockData: StockProps[], setStockData: any, item: StockProps) => {
+  const withdrawOne = (
+    stockData: StockProps[],
+    setStockData: any,
+    item: StockProps
+  ) => {
     item.amount > 0 ? (item.amount -= 1) : 0;
     const updatedItem = { ...item, amount: item.amount };
     const newList = stockData.filter((listItem) => listItem !== item);
     const updatedNewList = [...newList, updatedItem];
-    const sortedUpdatedNewList = updatedNewList.sort((a, b) => a.amount - b.amount);
+    const sortedUpdatedNewList = updatedNewList.sort(
+      (a, b) => a.amount - b.amount
+    );
     setStockData(sortedUpdatedNewList);
     localStorage.setItem("stock", JSON.stringify(sortedUpdatedNewList));
-    addStockLogEntry(item.productName, item.productId, 1)
-    console.log(item.productName, item.productId, 1)
+    addStockLogEntry(item.productName, item.productId, 1);
+    console.log(item.productName, item.productId, 1);
   };
 
   if (!stock) {
@@ -69,132 +83,173 @@ export const StockDetail: FC = () => {
   }
 
   return (
-    <Box sx={{
-      backgroundImage: "url('https://myguitarlessons.co.uk/wp-content/uploads/2014/10/Kleinteilelag-Thomann-factory-620x360.jpg')",
-      backgroundSize: "cover",
-      backgroundPosition: "center",
-      padding: "16px",
-      backdropFilter: "blur(8px)"
-    }}>
+    <Box
+      sx={{
+        backgroundImage:
+          "url('https://myguitarlessons.co.uk/wp-content/uploads/2014/10/Kleinteilelag-Thomann-factory-620x360.jpg')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        padding: "16px",
+        backdropFilter: "blur(8px)",
+      }}
+    >
       <Box
-  sx={{
-    backgroundColor: "rgba(211, 211, 211, 0.5)",
-    backdropFilter: "blur(10px)",
-    borderRadius: "8px",
-    display: "flex",
-    alignItems: "center",
-    marginBottom: "16px",
-    color: "white",
-    textAlign: "right",
-    marginRight: 20,
-    marginLeft: 20
-  }}
->
-  <img
-  src={stockImageMap[stockId]}
-  alt="Stock"
-  style={{ height: "128px", objectFit: "cover", marginLeft: 50 }}
-/>
-  <Typography variant="h2" sx={{ flexGrow: 1, marginRight: 5 }}>
-    {stockLists[parseInt(stockId) - 1].name}
-  </Typography>
-</Box>
+        sx={{
+          backgroundColor: "rgba(211, 211, 211, 0.5)",
+          backdropFilter: "blur(10px)",
+          borderRadius: "8px",
+          display: "flex",
+          alignItems: "center",
+          marginBottom: "16px",
+          color: "white",
+          textAlign: "right",
+          marginRight: 20,
+          marginLeft: 20,
+        }}
+      >
+        <img
+          src={stockImageMap[stockId]}
+          alt="Stock"
+          style={{ height: "128px", objectFit: "cover", marginLeft: 50 }}
+        />
+        <Typography variant="h2" sx={{ flexGrow: 1, marginRight: 5 }}>
+          {stockLists[parseInt(stockId) - 1].name}
+        </Typography>
+      </Box>
 
-
-      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", marginBottom: "16px", backgroundColor: "rgba(211, 211, 211, 0.5)",
-        backdropFilter: "blur(10px)",
-        borderRadius: "8px", color: "white" }}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          marginBottom: "16px",
+          backgroundColor: "rgba(211, 211, 211, 0.5)",
+          backdropFilter: "blur(10px)",
+          borderRadius: "8px",
+          color: "white",
+        }}
+      >
         <Typography variant="h4">STOCK DETAIL</Typography>
-        <Button variant="outlined" sx={{ color: "white", borderColor: "white"}} onClick={() => goToRequestPage(stockId)}>
+        <Button
+          variant="outlined"
+          sx={{ color: "white", borderColor: "white" }}
+          onClick={() => goToRequestPage(stockId)}
+        >
           Request
         </Button>
       </Box>
       <Grid container spacing={0} sx={{ marginBottom: "16px" }}>
-  <Grid item xs={1}>
-    <Typography variant="h6" sx={{
-      fontWeight: "bold",
-      backgroundColor: "rgba(211, 211, 211, 0.5)",
-      backdropFilter: "blur(10px)",
-      borderRadius: "8px",
-      color: "white",
-      textAlign: "center" }}>
-      stockId
-    </Typography>
-  </Grid>
-  <Grid item xs={2}>
-    <Typography variant="h6" sx={{
-      fontWeight: "bold",
-      backgroundColor: "rgba(211, 211, 211, 0.5)",
-      backdropFilter: "blur(10px)",
-      borderRadius: "8px",
-      color: "white",
-      textAlign: "center" }}>
-      productId
-    </Typography>
-  </Grid>
-  <Grid item xs={2}>
-    <Typography variant="h6" sx={{
-      fontWeight: "bold",
-      backgroundColor: "rgba(211, 211, 211, 0.5)",
-      backdropFilter: "blur(10px)",
-      borderRadius: "8px",
-      color: "white",
-      textAlign: "center" }}>
-      productName
-    </Typography>
-  </Grid>
-  <Grid item xs={2}>
-    <Typography variant="h6" sx={{
-      fontWeight: "bold",
-      backgroundColor: "rgba(211, 211, 211, 0.5)",
-      backdropFilter: "blur(10px)",
-      borderRadius: "8px",
-      color: "white",
-      textAlign: "center" }}>
-      amount
-    </Typography>
-  </Grid>
-  <Grid item xs={2} />
-</Grid>
-{stockById.map((item, index) => (
-  <Grid
-    key={index}
-    container
-    spacing={0}
-    sx={{
-      marginBottom: "8px",
-      backgroundColor: index % 2 === 0 ? "lightblue" : "lightgray",
-      padding: "8px",
-      borderRadius: "4px",
-      marginRight: 40,
-    }}
-  >
-    <Grid item xs={1}>
-      <Typography sx={{ textAlign: "center" }}>{item.stockId}</Typography>
-    </Grid>
-    <Grid item xs={2}>
-      <Typography sx={{ textAlign: "center" }}>{item.productId}</Typography>
-    </Grid>
-    <Grid item xs={2}>
-      <Typography sx={{ textAlign: "center" }}>{item.productName}</Typography>
-    </Grid>
-    <Grid item xs={2}>
-      <Typography sx={{ textAlign: "center", fontSize: 22 }}>{item.amount}</Typography>
-    </Grid>
-    <Grid item xs={2}>
-      <Button variant="contained" sx={{
-        backgroundColor: "rgba(211, 211, 211, 0.5)",
-        backdropFilter: "blur(10px)",
-        borderRadius: "8px",
-        color: "black",
-        cursor: item.amount > 0 ? "pointer" : "not-allowed"
-      }}
-        disabled={item.amount === 0}
-        onClick={() => withdrawOne(stockData, setStockData, item)}>Withdraw</Button>
-    </Grid>
-  </Grid>
-))}
-
+        <Grid item xs={1}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              backgroundColor: "rgba(211, 211, 211, 0.5)",
+              backdropFilter: "blur(10px)",
+              borderRadius: "8px",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            stockId
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              backgroundColor: "rgba(211, 211, 211, 0.5)",
+              backdropFilter: "blur(10px)",
+              borderRadius: "8px",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            productId
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              backgroundColor: "rgba(211, 211, 211, 0.5)",
+              backdropFilter: "blur(10px)",
+              borderRadius: "8px",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            productName
+          </Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: "bold",
+              backgroundColor: "rgba(211, 211, 211, 0.5)",
+              backdropFilter: "blur(10px)",
+              borderRadius: "8px",
+              color: "white",
+              textAlign: "center",
+            }}
+          >
+            amount
+          </Typography>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
+      {stockById.map((item, index) => (
+        <Grid
+          key={index}
+          container
+          spacing={0}
+          sx={{
+            marginBottom: "8px",
+            backgroundColor: index % 2 === 0 ? "lightblue" : "lightgray",
+            padding: "8px",
+            borderRadius: "4px",
+            marginRight: 40,
+          }}
+        >
+          <Grid item xs={1}>
+            <Typography sx={{ textAlign: "center" }}>{item.stockId}</Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography sx={{ textAlign: "center" }}>
+              {item.productId}
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography sx={{ textAlign: "center" }}>
+              {item.productName}
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography sx={{ textAlign: "center", fontSize: 22 }}>
+              {item.amount}
+            </Typography>
+          </Grid>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              sx={{
+                backgroundColor: "rgba(211, 211, 211, 0.5)",
+                backdropFilter: "blur(10px)",
+                borderRadius: "8px",
+                color: "black",
+                cursor: item.amount > 0 ? "pointer" : "not-allowed",
+              }}
+              disabled={item.amount === 0}
+              onClick={() => withdrawOne(stockData, setStockData, item)}
+            >
+              Withdraw
+            </Button>
+          </Grid>
+        </Grid>
+      ))}
     </Box>
   );
 };
